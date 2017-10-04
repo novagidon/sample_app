feef8049cab731d9b157df73a698cb8fa8a181ed
novagidon <novagidon@gmail.com>
2017-10-05 02:46:08 +0300

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
 
   def home
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+
   end
 
   def help
@@ -9,6 +14,6 @@ class StaticPagesController < ApplicationController
   def about
   end
 
-    def contact
+  def contact
   end
 end
