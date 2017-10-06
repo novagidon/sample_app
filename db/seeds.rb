65e9d80ea6cb9786388519c47c33ea2f2a8291c0
novagidon <novagidon@gmail.com>
2017-10-06 15:40:33 +0300

    addmeadmin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.create!(name:  "Ivan Kravchuk",
-             email: "ivankravchuk@gmail.com",
-             password:              "nuttertools21",
-             password_confirmation: "nuttertools21",
+             email: "novagidon@gmail.com",
+             password:              "123456",
+             password_confirmation: "123456",
              admin: true)
 
 99.times do |n|
